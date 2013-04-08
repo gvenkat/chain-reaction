5c8cf2c3a4b20765591bb7004b6f02442be63801
Venkatakrishnan Ganesh <superpulse.x@gmail.com>
2013-04-09 01:31:51 +0530

    * Changed init conditions

--- chain-reaction.py
+++ chain-reaction.py
@@ -2,13 +2,14 @@
 import sys
 import pygame
 import time
+from pygame.locals import *
 
 sys.path.insert( 0, 'lib' )
 
 
 # constants
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 800
+HEIGHT = 600
 DRAW_GRIDS = True
 BACKGROUND_COLOR = None
 FOREGROUND_COLOR = None
@@ -18,8 +19,8 @@ GRID_COLOR = None
 pygame.init()
 
 # start display
-screen = pygame.display.set_mode( ( WIDTH, HEIGHT ) )
+screen = pygame.display.set_mode( ( WIDTH, HEIGHT ), HWSURFACE | DOUBLEBUF )
 
 # deal with the game
 while True:
-  time.sleep( 1 )
+  time.sleep( 10 )
